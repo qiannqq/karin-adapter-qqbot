1c93cd591caf6fa0526d0b20e2bbaa3057376e9c
千奈千祁 <2632139786@qq.com>
2024-04-28 14:33:15 +0800

    chore: 移除无用console

--- model/botInit.js
+++ model/botInit.js
@@ -140,7 +140,6 @@ export default class botInit {
               if(item.type === 'text') bodyContent.content += item.text
               if(item.type === 'image') return
             }
-            console.log(bodyContent)
             let body = {
               method: 'POST',
               headers: {
